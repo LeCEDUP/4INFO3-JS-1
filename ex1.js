9c1761ddb3f01835b6821ecb0fd4d175ef108e82
RafaelPD81 <jogozero@gmail.com>
2026-03-16 20:36:01 -0300

    ex1resposta

---
+++ ex1.js
@@ -0,0 +1,9 @@
+let nome = "Rafael";
+const anonasc =  1981;
+let idade = 2026 - anonasc;
+let isestudante = true;
+
+console.log("Rafael", nome, "(" + typeof nome + ")");
+console.log("1981", anonasc, "(" + typeof anoNascimento + ")");
+console.log("44", idade, "(" + typeof idade + ")");
+console.log("true", isestudante, "(" + typeof isEstudante + ")");
