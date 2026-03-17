1085599539d933dd61d555f292a3dde3fefdea8b
damasiomarlon67-sudo <damasiomarlon67@gmail.com>
2026-03-17 19:23:52 -0300

    exerJS2

--- ex6.js
+++ ex6.js
@@ -2,7 +2,8 @@
 const titulo = document.getElementById('titulo-dom');
 titulo.textContent = "DOM Manipulado!";
 // 2. Selecione o parágrafo pela sua classe e adicione a classe CSS "destaque" a ele.
-
+const paragrafo = document.querySelector('.texto-paragrafo');
+paragrafo.classList.add('destaque');
 // 3. Selecione o botão pelo seu ID. Adicione um event listener de 'click' a ele.
 
 // Quando o botão for clicado, mude o textContent do parágrafo para "Texto alterado pelo clique!".
