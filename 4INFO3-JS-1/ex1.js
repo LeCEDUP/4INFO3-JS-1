78a5a49f550b74a29ae8407ec149e374375cdcfb
peluseoni <peluseoni@gmail.com>
2026-03-18 20:53:41 -0300

    commit ex 1 correção

--- 4INFO3-JS-1/ex1.js
+++ 4INFO3-JS-1/ex1.js
@@ -3,7 +3,7 @@ let NomeGato = "Prince";
 const AnoNascimentoGato = 2010;
 
 let AnoAtual = new Date().getFullYear();
-let IdadeGato = AnoAtual - AnoNascimentoGato;
+let idadeGato = AnoAtual - AnoNascimentoGato;
 
 let eCastrado = true;
 
@@ -11,4 +11,3 @@ console.log(`Nome do Gato: ${NomeGato} (${typeof NomeGato})`);
 console.log(`Ano de Nascimento: ${AnoNascimentoGato} (${typeof AnoNascimentoGato})`);
 console.log(`Idade do Gato: ${IdadeGato} (${typeof IdadeGato})`);
 console.log(`É Castrado: ${eCastrado} (${typeof eCastrado})`);
-
