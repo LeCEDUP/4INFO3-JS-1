f98a798668e8b90558b7e832a3235455fc5fb2fe
peluseoni <peluseoni@gmail.com>
2026-03-18 20:19:27 -0300

    commit ex 2

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let num1 = 15, num2 = 5;
+console.log(`Soma: ${num1 + num2}, Subtração: ${num1 - num2}, Mult: ${num1 * num2}, Div: ${num1 / num2}`);
+console.log(`Maior? ${num1 > num2}, Igual? ${num1 === num2}`);
+
+let nota = 85;
+if (nota >= 90) console.log("A");
+else if (nota >= 80) console.log("B");
+else if (nota >= 70) console.log("C");
+else if (nota >= 60) console.log("D");
+else console.log("F");
