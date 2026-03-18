0b3d6f65d5b34c052605fc43ae02b742ab04a24e
peluseoni <peluseoni@gmail.com>
2026-03-18 20:19:56 -0300

    commit ex 4

---
+++ ex4.js
@@ -0,0 +1,4 @@
+const somar = (a, b) => a + b;
+const saudacao = (n = "Visitante") => console.log(`Olá, ${n}!`);
+const ehPar = (n) => n % 2 === 0;
+const aplicarOperacao = (callback, a, b) => callback(a, b);
