84dea50dc8f87f532648acd2b2fd3a3a6dee3867
peluseoni <peluseoni@gmail.com>
2026-03-18 20:58:10 -0300

    commit ex 3 mudei os nomes, a idade e a cidade

--- ex3.js
+++ ex3.js
@@ -3,8 +3,8 @@ for (let i = 1; i <= 10; i++) console.log(i);
 let p = 0;
 while (p <= 20) { console.log(p); p += 2; }
 
-let nomes = ["Ana", "Bia", "Caio"];
+let nomes = ["Talia", "Lara", "Camili"];
 for (let n of nomes) console.log(n);
 
-let pessoa = { nome: "Ana", idade: 25, cidade: "SP" };
+let pessoa = { nome: "Talia", idade: 22, cidade: "JLLE" };
 for (let chave in pessoa) console.log(`${chave}: ${pessoa[chave]}`);
