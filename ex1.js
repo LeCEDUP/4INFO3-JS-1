678ff1c6ec8945a34ef9e44b778be30204918e34
peluseoni <peluseoni@gmail.com>
2026-03-18 20:19:10 -0300

    commit ex 1

---
+++ ex1.js
@@ -0,0 +1,13 @@
+let NomeGato = "Prince";
+
+const AnoNascimentoGato = 2010;
+
+let AnoAtual = new Date().getFullYear();
+let idadeGato = AnoAtual - AnoNascimentoGato;
+
+let eCastrado = true;
+
+console.log(`Nome do Gato: ${NomeGato} (${typeof NomeGato})`);
+console.log(`Ano de Nascimento: ${AnoNascimentoGato} (${typeof AnoNascimentoGato})`);
+console.log(`Idade do Gato: ${IdadeGato} (${typeof IdadeGato})`);
+console.log(`É Castrado: ${eCastrado} (${typeof eCastrado})`);
