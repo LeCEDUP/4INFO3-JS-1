6420d2f0cb8195bc4659e37f959a61ad742d2c7b
Joaosouza0612 <joaop.desouzam@gmail.com>
2026-03-18 21:01:44 -0300

    Realizado exercicio 1

---
+++ Ex1.js
@@ -0,0 +1,13 @@
+let nome = "João";
+
+const anoNascimento = 2002; 
+
+let anoAtual = 2026;
+let idade = anoAtual - anoNascimento;
+
+let isEstudante = true;
+
+console.log("Nome: " + nome + " (" + typeof nome + ")");
+console.log("Ano de Nascimento: " + anoNascimento + " (" + typeof anoNascimento + ")");
+console.log("Idade: " + idade + " (" + typeof idade + ")");
+console.log("É Estudante: " + isEstudante + " (" + typeof isEstudante + ")");
